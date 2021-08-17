8233f0c9a20129368f1e3389e049aaf41d91a112
Sebastian Ruiz <ruizju@javeriana.edu.co>
2021-08-17 00:13:38 -0500

    Pantallas terminadas, falta maps

--- app/src/main/java/com/example/easykit/AgregarProductoActivity.java
+++ app/src/main/java/com/example/easykit/AgregarProductoActivity.java
@@ -3,12 +3,16 @@ package com.example.easykit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AgregarProductoActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_producto);
+
     }
 }
