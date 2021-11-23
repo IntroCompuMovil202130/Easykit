e5dd8d43d0a1b324dce728b9618ae9e20c20fc1a
Cristian Da Camara <cristianj.dacamaras@javeriana.edu.co>
2021-11-23 14:43:29 -0500

    Usuario subido a la base de datos

--- app/src/main/java/com/example/easykit/RegistroActivity.java
+++ app/src/main/java/com/example/easykit/RegistroActivity.java
@@ -9,17 +9,27 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.models.Usuario;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class RegistroActivity extends AppCompatActivity {
 
     private EditText nombre,apellido,email,password;
     private FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    FirebaseStorage storage;
+    StorageReference mStorageRef;
+    public static final String PATH_USERS = "usuarios/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +40,16 @@ public class RegistroActivity extends AppCompatActivity {
         email = findViewById(R.id.textCorreo);
         password = findViewById(R.id.textPassword);
         mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+        mStorageRef = storage.getReference();
 
 
     }
 
     public void registro(View view) {
+        String nombreS = nombre.getText().toString();
+        String apellidoS = apellido.getText().toString();
         String correo = email.getText().toString();
         String contra = password.getText().toString();
         mAuth.createUserWithEmailAndPassword(correo ,contra).addOnCompleteListener(RegistroActivity.this, new OnCompleteListener<AuthResult>() {
@@ -45,6 +60,15 @@ public class RegistroActivity extends AppCompatActivity {
                 }else{
                     FirebaseUser user = mAuth.getCurrentUser();
                     if(user!=null){
+                        Usuario usuario = new Usuario();
+                        usuario.setNombre(nombreS);
+                        usuario.setApellido(apellidoS);
+                        usuario.setEmail(correo);
+                        usuario.setPassword(contra);
+
+                        myRef = database.getReference(PATH_USERS+ user.getUid());
+                        myRef.setValue(usuario);
+
                         UserProfileChangeRequest.Builder upcrb = new UserProfileChangeRequest.Builder();
                         upcrb.setDisplayName(correo);
                         user.updateProfile(upcrb.build());
